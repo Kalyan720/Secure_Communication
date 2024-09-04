2cc8f3c06092f977e535dd0c2dd33544f00fd881
Sai kalyan Kamisetti <119833878+Kalyan720@users.noreply.github.com>
2024-09-04 12:54:51 +0530

    Encryption
    This is the basic Draft

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Encryption{
+    Scanner PlainText = new Scanner(System.in);
+
+}
+public class Main{
+    public static void main{String[] args}:
+     Encryption e = new Encryption();
+}
